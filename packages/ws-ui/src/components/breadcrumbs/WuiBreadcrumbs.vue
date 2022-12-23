bffd219a40109d9cea0ac3a34ce928ddc2b7657c
guxukai <gxk@33.cn>
2022-12-23 09:13:19 +0800

    chore: remove ws-ui;

--- packages/ws-ui/src/components/breadcrumbs/WuiBreadcrumbs.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="wui-breadcrumbs">
-    <slot />
-  </div>
-</template>
-
-<style lang="css">
-@import url('breadcrumbs.css');
-</style>
--- packages/ws-ui/src/components/textarea/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as WuiTextarea } from './WuiTextarea.vue'
-export { default as WuiFormTextarea } from './WuiFormTextarea.vue'
